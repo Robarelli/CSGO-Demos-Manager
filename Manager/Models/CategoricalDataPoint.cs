dd9944578b19498ade21a8456fe8c047fbcf6722
Jim Hume <jim@ethor.com>
2020-06-29 22:02:48 -0700

    Adding CategoricalDataPoint to substitute Telerik class

---
+++ Manager/Models/CategoricalDataPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Models
+{
+    public class CategoricalDataPoint
+    {
+        public string Category { get; set; }
+        public double Value { get; set; }
+        public string Label { get; set; }
+    }
+}
--- Manager/ViewModel/Accounts/AccountWeaponsViewModel.cs
+++ Manager/ViewModel/Accounts/AccountWeaponsViewModel.cs
@@ -6,12 +6,12 @@ using GalaSoft.MvvmLight.CommandWpf;
 using GalaSoft.MvvmLight.Messaging;
 using GalaSoft.MvvmLight.Threading;
 using Manager.Messages;
+using Manager.Models;
 using Manager.ViewModel.Shared;
 using Manager.Views.Accounts;
 using Manager.Views.Demos;
 using Services.Interfaces;
 using Services.Models.Stats;
-using Telerik.Charting;
 
 namespace Manager.ViewModel.Accounts
 {
